385121f34c4a700df836f1c0b88cd3e4f54cee6f
omkar shinde <oshinde796@gmail.com>
2022-01-29 12:20:11 +0530

    deploy bug fix 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.scss";
 // import { Homepage } from "./pages/Homepage.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Navbar } from "./componants/navbar/Navbar.js";
+// import { Navbar } from "./componants/navbar/Navbar.js";
 import { Sidebar } from "./componants/sidebar/Sidebar.js";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+
+        {/* <Navbar /> */}
         <Sidebar />
+
         <Routes>{/* <Route index element={<Homepage />} /> */}</Routes>
       </BrowserRouter>
     </>
