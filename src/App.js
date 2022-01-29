b02e4b4057af92ec47a900dc7f64484c41931782
omkar shinde <oshinde796@gmail.com>
2022-01-29 18:08:47 +0530

    vercel error fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
 // import { Homepage } from "./pages/Homepage.js";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import { AllMails } from "./Pages/AllMails/AllMails";
+import { AllMails } from "./Pages/AllMails";
 
 import { Navbar } from "./Components/Navbar/Navbar.js";
 import { Sidebar } from "./Components/Sidebar/Sidebar.js";
